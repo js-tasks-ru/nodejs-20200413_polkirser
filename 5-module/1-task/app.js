c7ca69000173036dc1380fc1fceef7ba108b26de
Polkirser <poletaev@ucoz-team.net>
2020-04-30 16:54:05 +0300

    koa chat

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,38 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise((resolve) => {
+    subscribers.push(resolve);
+  });
+
+  let msg;
+  try {
+    msg = await promise;
+  } catch (err) {
+    throw err;
+  }
+
+  ctx.body = msg;
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const msg = ctx.request.body.message;
+
+  if (!msg) {
+    ctx.throw(400);
+  }
+
+  subscribers.forEach(function(resolve) {
+    resolve(String(msg));
+  });
+
+  subscribers = [];
+
+
 });
 
 app.use(router.routes());
